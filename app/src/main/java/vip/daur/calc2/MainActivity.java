7d19761155c1d0679ba89b6bc87d780b05b0f577
upagka <upagka@yandex.com>
2021-11-17 21:17:11 +0600

    Initial commit

---
+++ app/src/main/java/vip/daur/calc2/MainActivity.java
@@ -0,0 +1,14 @@
+package vip.daur.calc2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
