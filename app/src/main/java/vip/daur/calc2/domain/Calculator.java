41b6921db168f8a0915f805a3375dbb6ad56d3a4
upagka <upagka@yandex.com>
2021-11-21 18:17:03 +0600

    added some classes

--- app/src/main/java/vip/daur/calc2/domain/Calculator.java
+++ app/src/main/java/vip/daur/calc2/domain/Calculator.java
@@ -1,5 +1,5 @@
 package vip.daur.calc2.domain;
 
 public interface Calculator {
-
+    double performOperation(double argOne, double argTwo, Operation operation);
 }
---
+++ app/src/main/java/vip/daur/calc2/domain/Operation.java
@@ -0,0 +1,8 @@
+package vip.daur.calc2.domain;
+
+public enum Operation {
+    SUM,
+    SUB,
+    MULTI,
+    DIV
+}
